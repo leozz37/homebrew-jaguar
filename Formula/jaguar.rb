9102adab084528628c974de145f15e11c6a25345
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-11-18 07:54:38 -0300

    Update class formula

--- Formula/jaguar.rb
+++ Formula/jaguar.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Espanso < Formula
+class Jaguar < Formula
     desc "CLI tool for testing socket connections"
     homepage "https://github.com/leozz37/jaguar"
     url "https://github.com/leozz37/jaguar/releases/download/v0.1/jaguar.tar.gz"
